bc522dfbde7dd15d84f53f949fbc5250d1b1678f
Dev Perfecular <dev.perfacular@gmail.com>
2012-10-18 09:15:13 -0700

    One Stage

--- src/com/antonio081014/keychains/entity/ProgramList.java
+++ src/com/antonio081014/keychains/entity/ProgramList.java
@@ -3,7 +3,6 @@
  */
 package com.antonio081014.keychains.entity;
 
-import android.annotation.SuppressLint;
 import java.util.List;
 
 /**
@@ -13,48 +12,48 @@ import java.util.List;
 
 public class ProgramList {
 
-	private int _PLID;
-	private List<Integer> _programList;
+    private int _PLID;
+    private List<Integer> _programList;
 
-	/**
-	 * @param _programList
-	 */
-	public ProgramList(List<Integer> _programList) {
-		this._programList = _programList;
-	}
+    /**
+     * @param _programList
+     */
+    public ProgramList(List<Integer> _programList) {
+	this._programList = _programList;
+    }
 
-	/**
-	 * @param _PLID
-	 * @param _programList
-	 */
-	public ProgramList(int _PLID, List<Integer> _programList) {
-		this._PLID = _PLID;
-		this._programList = _programList;
-	}
+    /**
+     * @param _PLID
+     * @param _programList
+     */
+    public ProgramList(int _PLID, List<Integer> _programList) {
+	this._PLID = _PLID;
+	this._programList = _programList;
+    }
 
-	public int get_PLID() {
-		return _PLID;
-	}
+    public int get_PLID() {
+	return _PLID;
+    }
 
-	public void set_PLID(int _PLID) {
-		this._PLID = _PLID;
-	}
+    public void set_PLID(int _PLID) {
+	this._PLID = _PLID;
+    }
 
-	public List<Integer> get_programList() {
-		return _programList;
-	}
+    public List<Integer> get_programList() {
+	return _programList;
+    }
 
-	public void set_programList(List<Integer> _programList) {
-		this._programList = _programList;
-	}
+    public void set_programList(List<Integer> _programList) {
+	this._programList = _programList;
+    }
 
-	public void addProgram(int programID) {
-		if (this.get_programList().contains(programID))
-			return;
-		this.get_programList().add(programID);
-	}
+    public void addProgram(int programID) {
+	if (this.get_programList().contains(programID))
+	    return;
+	this.get_programList().add(programID);
+    }
 
-	public void deleteProgram(int programID) {
-		this.get_programList().remove(Integer.valueOf(programID));
-	}
+    public void deleteProgram(int programID) {
+	this.get_programList().remove(Integer.valueOf(programID));
+    }
 }
